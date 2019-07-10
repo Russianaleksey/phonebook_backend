8bf34dd7df4b86dafb4f699eefe475d14ca2d11b
Russianaleksey <alexmamontovprofessional@gmail.com>
2019-07-10 09:57:03 -0400

    phonebook backend

---
+++ index.js
@@ -0,0 +1,98 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+
+let persons = [
+    {
+        "name": "Arto Hellas",
+        "number": "040-123456",
+        id: 1
+    },
+    {
+        "name": "Ada Lovelace",
+        "number": "127-22301",
+        id: 2
+    },
+    {
+        "name": "Dan Abramov",
+        "number": "923-123951",
+        id: 3
+    },
+    {
+        "name": "Mary Poppendieck",
+        "number": "042-123456",
+        id: 4
+    },   
+]
+
+app.get('/', (req, res) => {
+    res.send('<h1>MAIN PAGE</h1>')
+})
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(per => per.id === id)
+    person ? res.json(person) : res.status(404).end()
+})
+
+const getInfo = () => {
+    return(
+        `
+        <div>
+            <h4>Phonebook has info for ${persons.length} people</h4>
+            <p>${new Date()}</p>
+        </div>
+        `
+    )
+}
+
+app.get('/info', (req, res) => {
+    const len = persons.length
+    res.send(getInfo())
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(per => per.id !== id)
+
+    res.status(204).end()
+})
+
+const getRandomID = () => {
+    return Math.floor((Math.random() * 1000000) + 1)
+}
+
+app.post('/api/persons', (req, res) => {
+    const body = req.body
+
+    if(!body.name || !body.number){
+        return res.status(400).json({
+            error: "name or number missing"
+        })
+    } else if(persons.find(per => per.name === body.name)){
+        return res.status(400).json({
+            error: "name must be unique"
+        })
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number || false,
+        id: getRandomID()
+    }
+
+    persons = persons.concat(person)
+
+    res.json(person)
+})
+
+const port = 3000
+app.listen(port, () => {
+    console.log(`server running on port ${port}`)
+})
