2c88662c97cc04cccf84f854d93d85f4c4a1cfeb
Mamontov <aam05@acuitysso.com>
2019-07-16 11:09:49 -0400

    uibuild

---
+++ build/precache-manifest.6ba662c4a3f8600a3a173cf0eb4d928b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d3f470163325d980280f6ee3c9eda6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "997b01c3eb528ebb9bd9",
+    "url": "/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "6a19a0beea039a280f26",
+    "url": "/static/js/2.3c62b441.chunk.js"
+  },
+  {
+    "revision": "997b01c3eb528ebb9bd9",
+    "url": "/static/js/main.41267455.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors())
 app.use(bodyParser.json())
 morgan.token('postBody', (req, res) => { return `${JSON.stringify(req.body)}` })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postBody'))
-
+app.use(express.static('build'))
 
 let persons = [
     {
