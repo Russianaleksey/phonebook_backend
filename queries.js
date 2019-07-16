f345b72acaa88306d312348c1638d6e864c50b57
russianaleksey <alexmamontovprofessional@gmail.com>
2019-07-16 19:50:24 -0400

    uibuild

--- queries.js
+++ queries.js
@@ -1,11 +1,19 @@
 const Pool = require('pg').Pool
+const url = require('url')
+
+const params = url.parse('postgres://nmyxibvbainedv:b879940902d0a7831bcfcbafbcf87253a8e581cb5bca32c1e8ceba2cbfda1f81@ec2-23-21-160-38.compute-1.amazonaws.com:5432/d7lck4qa8f3ei9')
+const auth = params.auth.split(':');
+
+
 const pool = new Pool({
-    user: 'me',
-    host: 'localhost',
-    database: 'phonebook_api',
-    password: 'password',
-    port: 5432,
+    user: auth[0],
+    password: auth[1],
+    host: params.hostname,
+    port: params.port,
+    database: params.pathname.split('/')[1],
+    ssl: true
 })
+
 
 const getPeople = (req, res) => {
     pool.query('SELECT * FROM people ORDER BY id ASC', (error, results) => {
